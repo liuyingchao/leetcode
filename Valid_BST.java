db0ebebe3628958ac4e8dd30c833392fcfbd9e5d
liuyingchao <liuyingchao@users.noreply.github.com>
2013-11-20 18:50:41 -0800

    Update Valid_BST.java

--- Valid_BST.java
+++ Valid_BST.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
-//$ TODO: the first solution exceeds memeory limit. The second is wrong for {1, 1}, which should return false.
-// Still debugging.
+
+//$ TODO: bug--returns false for {0, -1}
 /**
  * Definition for binary tree
  * public class TreeNode {
@@ -10,53 +10,26 @@ import java.util.ArrayList;
  *     TreeNode(int x) { val = x; }
  * }
  */
-/*
- * public class Solution {
-	public boolean isValidBST(TreeNode root) {
-		ArrayList<Integer> al =  new ArrayList<Integer>();
-		serielizeTree(root, al);
-		int N = al.size();
-		if (N <= 1) {
-			return true;
-		}
-		for (int i = 0; i < N - 1; i++) {
-			if (al.get(i) >= al.get(i+1)) {
-				return false;
-			}
-		}
-		return true;
-    }
-	
-	private void serielizeTree(TreeNode root, ArrayList<Integer> al) {
-		if (root != null) {
-			serielizeTree(root.left, al);
-			al.add(root.val);
-			serielizeTree(root.left, al);
-		}
-	}
-}
- * */
 public class Solution {
+	private int prev = Integer.MIN_VALUE;
 	public boolean isValidBST(TreeNode root) {
 		if (root == null) return true;
-		return isValid(root, Integer.MIN_VALUE);
+		return isValid(root);
     }
 	
-	private boolean isValid(TreeNode root, int prev) {
-		boolean valid = true;
-		if (root.left != null) {
-			valid = isValid(root.left, prev);
-		}
-		valid = valid && root.val > prev;
-		prev = root.val;
-		if (valid) {
-			if (root.right != null) {
-				return isValid(root.right, prev);
-			} else {
-				return true;
-			}
-		} else {
+	private boolean isValid(TreeNode root) {
+		if (root == null) return true;
+		
+		if (root.left != null && !isValid(root.left)) {
 			return false;
-		}		
+		}
+		if (root.val <= prev) return false;
+		
+		prev = root.val;
+		
+		if (root.right != null && !isValid(root.left)) {
+			return false;
+		}
+		return true;	
 	}
 }
