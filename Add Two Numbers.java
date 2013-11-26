37d4bbb7c5f431a12832b954f9f85f796f01cf54
liuyingchao <liuyingchao@users.noreply.github.com>
2013-11-25 22:29:22 -0800

    Create Add Two Numbers.java
    You are given two linked lists representing two non-negative numbers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
    
    Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
    Output: 7 -> 0 -> 8

---
+++ Add Two Numbers.java
@@ -0,0 +1,54 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+ 
+// My NOTES: there is code duplicate in those 2 while loops, but it's not easy to refactor.
+// I'll revisit.
+public class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        if (l1 == null && l2 == null) {
+        	return null;
+        }
+        int sum, value;
+        int carry = 0;
+        ListNode dummy = new ListNode(0);
+        ListNode prev = dummy;
+        ListNode current;
+        while (l1 != null && l2 != null) {
+        	sum = l1.val + l2.val + carry;
+        	carry = sum / 10;
+        	value = sum % 10;
+        	current =  new ListNode(value);
+        	prev.next = current;
+        	prev = current;
+        	l1 = l1.next;
+        	l2 = l2.next;
+        }
+        
+        ListNode remain = l1 == null ? l2 : l1;
+        while (remain != null) {
+        	sum = remain.val + carry;
+        	carry = sum / 10;
+        	value = sum % 10;
+        	current = new ListNode(value);
+        	prev.next = current;
+        	prev = current;
+        	remain = remain.next;
+        }
+        
+        if (carry == 1) {
+        	current = new ListNode(1);
+        	prev.next = current;
+        }
+        
+        return dummy.next;
+    }
+}
