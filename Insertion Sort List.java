290cfe8b1ddc6736a7e4acaa2b352f9117d16dcd
liuyingchao <liuyingchao@users.noreply.github.com>
2013-12-05 00:39:33 -0800

    Update Insertion Sort List.java
    refactor

--- Insertion Sort List.java
+++ Insertion Sort List.java
@@ -9,6 +9,36 @@
  *     }
  * }
  */
+// This version is an improvement that gets rid of "end" 
+public class Solution {
+    public ListNode insertionSortList(ListNode head) {
+        // head.next == null can be handled by the code after the first if branch,
+        // but it's not clear whether removing this OR operator will be better code
+    	if (head == null || head.next == null) {
+    		return head;
+    	}
+    	ListNode dummy = new ListNode(Integer.MIN_VALUE);
+    	dummy.next = head;
+    	ListNode start = dummy;
+    	ListNode current = head.next;
+    	head.next = null;
+    	while (current != null) {
+    		ListNode incoming = current.next;
+    		ListNode prev = start;
+    		ListNode loopNode = start.next;
+    		while (loopNode != null && loopNode.val < current.val) {
+    			prev = loopNode;
+    			loopNode = loopNode.next;
+    		}
+    		current.next = loopNode;
+    		prev.next = current;
+    		
+    		current = incoming;
+    	}
+    	return dummy.next;
+    }
+} 
+ 
 public class Solution {
     public ListNode insertionSortList(ListNode head) {
     	if (head == null || head.next == null) {
