de45589051bf742b223c36169787306bea625a8c
liuyingchao <liuyingchao@users.noreply.github.com>
2014-01-14 22:38:49 -0800

    Update Reorder List.java
    Adding the O(1) solution

--- Reorder List.java
+++ Reorder List.java
@@ -11,6 +11,65 @@ import java.util.*;
  * }
  */
  
+// O(1) space solution
+public class Solution {
+    public void reorderList(ListNode head) {
+    	ListNode backup = head;
+    	if (head != null && head.next != null) {
+    		// count
+    		int total = 0;
+            while (head != null) {
+            	head = head.next;
+            	total++;
+            }
+            // find tail
+            int half = (total+1)/2;
+            head = backup;
+            for (int i = 0; i < half - 1; i++) {
+            	head = head.next;
+            }            
+            ListNode tail = head;
+            
+            // Break the list into half
+            ListNode secondHead = new ListNode(0);
+            secondHead.next = tail.next;
+            tail.next = null;
+            
+            // Reverse the second half
+            ListNode secondTail = secondHead.next;
+            ListNode node = secondTail;
+            ListNode nextNode = node.next;
+            int secondHalf = total - half;
+            // There are at least 2 nodes in the original list, so secondHalf is at least 1
+            // If secondHalf is 1, we are already good
+            for (int i = 0; i < secondHalf - 1; i++) {
+            	node = nextNode;
+            	nextNode = node.next;
+            	node.next = secondHead.next;
+            	secondHead.next = node;
+            }
+            secondTail.next = null;
+            
+            // Merge
+            head = backup;
+            node = secondHead.next;
+            ListNode topNext, secondNext;
+            // Make sure we use the second list as the loop invariant, in case the first list is longer by one node.
+            while (node != null) {	
+            	topNext = head.next;
+            	secondNext = node.next;
+            	node.next= topNext;
+            	head.next = node;
+            	head = topNext;
+            	node = secondNext;
+            }
+            
+            head = backup;
+            return;
+    	}
+    }
+}
+ 
  // MY NOTE: this code actually uses additional O(n) space. The real "in-place"
  // solution is running the same logic, just starting with fast/slow pointers,
  // and then do list reverse on the second half, and then merge the 2. Just
