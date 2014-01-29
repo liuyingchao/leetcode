8cf5307204e1cd3a7166d3e6bb44a29c2445166f
liuyingchao <liuyingchao@users.noreply.github.com>
2014-01-28 18:17:00 -0800

    Update Palindrome Partitioning.java
    comments

--- Palindrome Partitioning.java
+++ Palindrome Partitioning.java
@@ -1,3 +1,18 @@
+/* 
+MY NOTE: look deep into it. Recursion and Combination
+Given a string s, partition s such that every substring of the partition is a palindrome.
+
+Return all possible palindrome partitioning of s.
+
+For example, given s = "aab",
+Return
+
+  [
+    ["aa","b"],
+    ["a","a","b"]
+  ]
+ * */
+ 
 public class Solution {
     public ArrayList<ArrayList<String>> partition(String s) {
         // Start typing your Java solution below
