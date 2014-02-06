c20e1b8c1722b10523ed61fc3129c791b6416af7
liuyingchao <liuyingchao@users.noreply.github.com>
2014-02-05 21:51:33 -0800

    Add the fast slower pointer solution for findTail

--- Reorder List.java
+++ Reorder List.java
@@ -3,6 +3,85 @@
  * which means the old head still points to some old value in the middle of the list. That's the root cause of my initial bugs.
  * */
 
+// The O(1) space solution to use slow and fast pointers to avoid counting. However,
+// traversing with 2 pointers have roughly the same running time as the counting solution.
+import java.util.*;
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public void reorderList(ListNode head) {
+    	ListNode backup = head;
+    	if (head != null && head.next != null) {
+    		ListNode tail = findTail(head);
+    		// Break into 2 halves
+            ListNode secondHead = tail.next;
+            // Then reverse
+            secondHead = reverseList(secondHead);
+            tail.next = null;
+
+            // Merge
+            head = backup;
+            head = mergeLists(head, secondHead);
+            return;
+    	}
+    }
+    
+    private ListNode findTail(ListNode head) {
+    	ListNode dummy = new ListNode(0);
+    	dummy.next = head;
+    	ListNode fast = dummy;
+    	ListNode slow = dummy;
+    	while (fast != null && fast.next != null) {
+    		fast = fast.next.next;
+    		slow = slow.next;
+    	}
+    	if (fast == null) {
+    		return slow;
+    	} else {
+    		return slow.next;
+    	}
+    }
+    
+    private ListNode reverseList(ListNode head) {
+        ListNode dummy = new ListNode(0);
+        while (head != null) {
+        	ListNode temp = head.next;
+        	head.next = dummy.next;
+        	dummy.next = head;
+        	head = temp;
+        }
+    	
+        return dummy.next;
+    }
+    
+    private ListNode mergeLists(ListNode firstHead, ListNode secondHead) {
+    	ListNode backup = firstHead;
+    	ListNode node = secondHead;
+        ListNode firstNext, secondNext;
+        // Make sure we use the second list as the loop invariant, in case the first list is longer by one node.
+        while (node != null) {	
+        	firstNext = firstHead.next;
+        	secondNext = node.next;
+        	node.next= firstNext;
+        	firstHead.next = node;
+        	firstHead = firstNext;
+        	node = secondNext;
+        }
+        return backup;
+    }
+}
+
+// The O(1) space solution by counting.
+
 import java.util.*;
 /**
  * Definition for singly-linked list.
