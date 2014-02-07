9801d96475d410aa5e23f72f6d20a57769ad4eed
liuyingchao <liuyingchao@users.noreply.github.com>
2014-02-06 21:41:11 -0800

    Add the O(1) space solution

--- Single_Number_II.java
+++ Single_Number_II.java
@@ -1,3 +1,25 @@
+/*Given an array of integers, every element appears three times except for one. Find that single one.
+
+Note:
+Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+ * */
+
+public class Solution {
+    public static int singleNumber(int[] A) {  
+        int[] countsPerBit = new int[32];  
+        int result = 0;  
+        for(int i=0; i<32; i++){  
+            for(int j=0; j<A.length; j++){  
+                if(((A[j] >> i) & 1) == 1){  
+                    countsPerBit[i] = (countsPerBit[i] + 1) % 3;  
+                }  
+            }  
+            result |= (countsPerBit[i] << i);  
+        }  
+        return result;  
+    }  
+}
+
 import java.util.HashMap;
 
 public class Solution {
