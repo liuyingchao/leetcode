1a9838edcb947a0d43656dc163657c4d3c9095d8
liuyingchao <liuyingchao@users.noreply.github.com>
2014-02-09 14:47:14 -0800

    Update Longest Consecutive Sequence.java

--- Longest Consecutive Sequence.java
+++ Longest Consecutive Sequence.java
@@ -6,6 +6,10 @@ Given [100, 4, 200, 1, 3, 2],
 The longest consecutive elements sequence is [1, 2, 3, 4]. Return its length: 4.
 
 Your algorithm should run in O(n) complexity.
+
+Solution: use avaialable dataSet to track what numbers are in the array, then scan for the second
+time by using a visited hashSet to assist "discovering" from a new number to both sides.
+
  * */
 import java.util.*;
 public class Solution {
