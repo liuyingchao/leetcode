e63f108e84bbe3659a179dd56f0a25831a4d840b
liuyingchao <liuyingchao@users.noreply.github.com>
2014-02-08 23:08:50 -0800

    Add the backgrack Java solution based on C++

--- Wildcard Matching.java
+++ Wildcard Matching.java
@@ -1,3 +1,78 @@
+/*
+ * Implement wildcard pattern matching with support for '?' and '*'.
+
+'?' Matches any single character.
+'*' Matches any sequence of characters (including the empty sequence).
+
+The matching should cover the entire input string (not partial).
+
+The function prototype should be:
+bool isMatch(const char *s, const char *p)
+
+Some examples:
+isMatch("aa","a") → false
+isMatch("aa","aa") → true
+isMatch("aaa","aa") → false
+isMatch("aa", "*") → true
+isMatch("aa", "a*") → true
+isMatch("ab", "?*") → true
+isMatch("aab", "c*a*b") → false
+
+Solution: crazy backtracking to outperforme bottom-up DP. There ought to be a top-down DP method if we can distinguish between true, false and unknown states.
+ * */
+public class Solution {
+    public boolean isMatch(String s, String p) {
+        if (s == null || s.length() == 0) {
+            return isFullStar(p);
+        }
+        if (p == null || p.length() == 0) {
+            return false;
+        }
+        
+        int m = s.length(), n = p.length();
+        int sBackup = -1, pBackup = -1;
+        int i = 0, j = 0;
+        while (i < m) {
+        	if (j < n && (p.charAt(j) == '?' || s.charAt(i) == p.charAt(j))) {
+        		i++;
+        		j++;
+        		// Do NOT use i==m to return value here. Allow i to increase if pBackup is a good point to stop j.
+        	} else if (j < n && p.charAt(j) == '*') {
+        		// Jump over all the consequtive '*'
+        		while (j < n && p.charAt(j) == '*') {
+        			j++;
+        		}
+        		if (j == n) return true;
+        		// We can safely back up here, because the last else branch moving forward on s, but keep p on the last known '*' plus 1 until we are ready to moving both pointers forward or reach the end.
+        		sBackup = i;
+        		pBackup = j;
+        	} else {
+        		if (sBackup < 0) return false;
+        		// The following part is backtracking on s, while keeping p on the last known '*' plus 1
+        		sBackup++;
+        		i = sBackup;
+        		j = pBackup;
+        	}        	
+        }
+        
+        while (j < n && p.charAt(j) == '*') {
+        	j++;
+        }
+        
+        return i == m && j == n;        
+    }
+    
+    private boolean isFullStar(String s) {
+        if (s == null || s.length() == 0) return true;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != '*') {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
 /* The following DP solution times out on extremely long inputs, as it builds a lot of redundant table cells
 */
 public class Solution {
