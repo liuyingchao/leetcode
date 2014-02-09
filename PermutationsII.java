4046ae8d24624deed749cedccee739284b473e8b
liuyingchao <liuyingchao@users.noreply.github.com>
2014-02-08 16:00:16 -0800

    Add another solution with hash.

--- PermutationsII.java
+++ PermutationsII.java
@@ -1,3 +1,48 @@
+/* Solution II; with hash. NOT elegant
+
+Very similar to original permutations without any duplicate elements. It just utilizes hash to avoid duplicate
+ * */
+import java.util.*;
+public class Solution {
+    public ArrayList<ArrayList<Integer>> permuteUnique(int[] num) {
+        if (num == null){
+            return null;
+        }
+        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+        
+        if (num.length == 0){
+            return result; 
+        }
+        
+        ArrayList<Integer> temp = new ArrayList<Integer>();
+        temp.add(num[0]);
+        result.add(temp);
+        
+        for (int i = 1; i < num.length; i++){
+            result = insert(num[i], result);
+        }
+        
+        return result;
+    }
+    
+    private ArrayList<ArrayList<Integer>> insert(int i, ArrayList<ArrayList<Integer>> result) {
+        ArrayList<ArrayList<Integer>> newResult = new ArrayList<ArrayList<Integer>>();
+        HashSet<ArrayList<Integer>> hash = new HashSet<ArrayList<Integer>>();
+        
+        for (ArrayList<Integer> current: result){
+            for (int j=0; j <= current.size(); j++){
+                ArrayList<Integer> temp = new ArrayList<Integer>();
+                temp.addAll(current);
+                temp.add(j, i);
+                if (!hash.containsKey(temp)){
+                    newResult.add(temp);
+                    hash.add(temp);
+                }
+            }
+        }
+       return newResult;
+    }
+}
 /*
  * Given a collection of numbers that might contain duplicates, return all possible unique permutations.
 
