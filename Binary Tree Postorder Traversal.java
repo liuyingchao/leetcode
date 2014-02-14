911971db27dabc322fcb7bd24c227a0de7d0f8f3
liuyingchao <liuyingchao@users.noreply.github.com>
2014-02-13 21:39:33 -0800

    Recursive. Not ideal

---
+++ Binary Tree Postorder Traversal.java
@@ -0,0 +1,38 @@
+/**
+Given a binary tree, return the postorder traversal of its nodes' values.
+
+For example:
+Given binary tree {1,#,2,3},
+   1
+    \
+     2
+    /
+   3
+return [3,2,1].
+
+Note: Recursive solution is trivial, could you do it iteratively?
+
+
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ArrayList<Integer> postorderTraversal(TreeNode root) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        helper(root, list);
+        return list;
+    }
+    
+    private void helper(TreeNode root, ArrayList<Integer> list) {
+        if (root != null) {
+            helper(root.left, list);
+            helper(root.right, list);
+            list.add(root.val);
+        }
+    }
+}
