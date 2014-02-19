3e3bbb6e5a3740284452345f63a9932417298a0c
liuyingchao <liuyingchao@users.noreply.github.com>
2014-02-19 10:51:30 -0800

    Rewrite with string comp and comments

--- Evaluate Reverse Polish Notation.java
+++ Evaluate Reverse Polish Notation.java
@@ -1,25 +1,49 @@
+/*
+Evaluate the value of an arithmetic expression in Reverse Polish Notation.
+
+Valid operators are +, -, *, /. Each operand may be an integer or another expression.
+
+Some examples:
+  ["2", "1", "+", "3", "*"] -> ((2 + 1) * 3) -> 9
+  ["4", "13", "5", "/", "+"] -> (4 + (13 / 5)) -> 6
+
+Difficulty: Easy
+  
+Solution: string comparion is little more verbose than try catch on NumberFormatException,
+but it's bad code practice to use Exception Handling for logic flow, and it performes worse.
+So I rewrite to this verbose version.
+*/
+
 import java.util.*;
 public class Solution {
     public int evalRPN(String[] tokens) {
         Stack stack =  new Stack();
         int operand1, operand2, current = 0;
+        char operator;
         for (String s : tokens) {
-        	try {
-        		current = Integer.parseInt(s);
+            if (!s.equals("+") && !s.equals("-") && !s.equals("*") && !s.equals("/")) {
+                current = Integer.parseInt(s);
         		stack.push(current);
-        	} catch (NumberFormatException ex) {
+            } else {
         		operand2 = (Integer) stack.pop();
     			operand1 = (Integer) stack.pop();
-        		if (s.equals("+")) {
-            		current = operand1 + operand2;  
-            	} else if (s.equals("-")) {
-            		current = operand1 - operand2;
-            	} else if (s.equals("*")) {
-            		current = operand1 * operand2;
-            	} else if (s.equals("/")) {
-            		current = operand1 / operand2;
-            	}
-        		stack.push(current);
+    			operator = s.charAt(0);
+    			switch (operator) {
+					case '+':
+					current = operand1 + operand2;
+					break;
+					case '-':
+					current = operand1 - operand2;
+					break;
+					case '*':
+					current = operand1 * operand2;
+					break;
+					case '/':
+					current = operand1 / operand2;
+					break;
+    			}
+    			
+    			stack.push(current);
         	}
         }
         return current;
