710957b3c9cb633877075629a7d9217ad46d66ee
liuyingchao <liuyingchao@users.noreply.github.com>
2014-02-20 10:48:13 -0800

    linear space solution

--- First Missing Positive.java
+++ First Missing Positive.java
@@ -7,6 +7,40 @@ and [3,4,-1,1] return 2.
 
 Your algorithm should run in O(n) time and uses constant space.
 
+Difficulty: Medium
+
+Solution: reuse A for bookkeeping. This uses constant space, and appears linear time.
+However, I'm not fully convinced that the if branch can be finished in constant time.
+Some special inputs might yield O(n^2) solution.
+*/
+
+public class Solution {
+    public int firstMissingPositive(int[] A) {
+        if (A == null || A.length == 0) return 1;
+        int n = A.length;
+        int i = 0;
+        while (i < n) {
+        	if (A[i] != i+1 && A[i] > 0 && A[i] <= n && A[A[i]-1] != A[i]) {
+        		swap(A, i, A[i]-1);
+        	} else {
+        		i++;
+        	}
+        }
+        
+        for (i = 0; i < n; i++) {
+        	if (A[i] != i+1) return i+1;
+        }
+        return n+1;
+    }
+    
+    private void swap(int[] A, int i, int j) {
+    	int temp = A[i];
+    	A[i] = A[j];
+    	A[j] = temp;
+    }
+}
+
+/*
 Solution: the idea is to use the fact that an array of elements naturally gives us
 a place to "bitmark" which number exists in the array. However, this solution uses O(n) space
 
