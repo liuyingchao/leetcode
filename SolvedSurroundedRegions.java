88b6985067c38b4a2ab1b54b2f3ef8871dc1a70c
liuyingchao <liuyingchao@users.noreply.github.com>
2014-02-23 11:42:27 -0800

    Comments and add the better code for BFS

---
+++ SolvedSurroundedRegions.java
@@ -0,0 +1,121 @@
+/*
+Given a 2D board containing 'X' and 'O', capture all regions surrounded by 'X'.
+
+A region is captured by flipping all 'O's into 'X's in that surrounded region .
+
+For example,
+X X X X
+X O O X
+X X O X
+X O X X
+After running your function, the board should be:
+
+X X X X
+X X X X
+X X X X
+X O X X
+
+Solution: the idea is described at: http://yucoding.blogspot.com/2013/08/leetcode-question-131-surrounded-regions.html
+The key observation is: instead of start BFS randomnly from inside the board, we start BFS from the boarders, which
+gives us a clear direction of what to do compared with treating everything 'O' the same and making decision in a messy way.
+Mark the result of BFS with temp char 'T', then it's trivial to flip 'O' and restore 'O'
+*/
+
+public class Solution {
+    private int m, n;
+    Queue<Integer> queue;
+    public void solve(char[][] board) {
+        if (board == null || board.length == 0 || board[0] == null || board[0].length == 0) return;
+        
+        queue = new LinkedList<Integer>();
+        m = board.length;
+        n = board[0].length;
+        for (int i = 0; i < m; i++) {
+            bfs(board, i, 0);
+            bfs(board, i, n-1);
+        }
+        for (int i = 1; i < n-1; i ++) {
+            bfs(board, 0, i);
+            bfs(board, m-1, i);
+        }
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 'O') {
+                    board[i][j] = 'X';
+                } else if (board[i][j] == 'T') {
+                    board[i][j] = 'O';
+                }
+            }
+        }
+    }
+    
+    private void bfs(char[][] board, int x, int y) {
+        check(board, x, y);
+        while (!queue.isEmpty()) {
+            int num = queue.poll();
+            x = num / n;
+            y = num % n;
+            check(board, x + 1, y);
+            check(board, x - 1, y);
+            check(board, x, y + 1);
+            check(board, x, y - 1);
+        }
+    }
+    
+    private void check(char[][] board, int x, int y) {
+        if (x < 0 || x >= m || y < 0 || y >= n || board[x][y] != 'O') return;
+        queue.add(x*n + y);
+        board[x][y] = 'T';
+    }
+}
+
+
+// BFS--copied from https://github.com/guolinaileen/abc/blob/master/Surrounded%20Regions_BFS.java
+public class Solution {
+    Queue<Integer> queue=new LinkedList<Integer>();
+    public void solve(char[][] board) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        if(board.length==0) return ; 
+        for(int i=0; i<board[0].length; i++)
+        {
+            process(board, 0, i);
+            process(board, board.length-1, i);
+        }
+        for(int i=0; i<board.length; i++)
+        {
+            process(board, i, 0);
+            process(board, i, board[0].length-1);
+        }
+        for(int i=0; i<board.length; i++)
+        {
+            for(int j=0; j<board[0].length; j++)
+            {
+                if(board[i][j]=='D') board[i][j]='O'; 
+                else if(board[i][j]=='O') board[i][j]='X';
+            }
+        }
+    }
+    void check(char[][] board, int i, int j)
+    {
+        if(i<0|| j<0 || i>=board.length||j>=board[0].length||board[i][j]!='O') return ; 
+        queue.offer(i*board[0].length+j);
+        board[i][j]='D';
+    }
+    void process(char[][] board, int i, int j)
+    {
+        check(board, i,  j); 
+        while(!queue.isEmpty())
+        {
+            int num=queue.poll();
+            i=num/board[0].length;
+            j=num%board[0].length;
+            check(board, i+1, j);
+            check(board, i-1, j);
+            check(board, i, j+1);
+            check(board, i, j-1);
+        }
+
+    }
+}
