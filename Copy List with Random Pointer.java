2ff82c3bf2a62e7d48291f456f0925afa8942690
liuyingchao <liuyingchao@users.noreply.github.com>
2014-03-01 11:09:39 -0800

    Add the solution without using hash

--- Copy List with Random Pointer.java
+++ Copy List with Random Pointer.java
@@ -1,3 +1,61 @@
+
+/**
+ * A linked list is given such that each node contains an additional random pointer which could point to any node in the list or null.
+
+Return a deep copy of the list.
+
+
+ * 
+ * Definition for singly-linked list with a random pointer.
+ * class RandomListNode {
+ *     int label;
+ *     RandomListNode next, random;
+ *     RandomListNode(int x) { this.label = x; }
+ * };
+ * 
+ * Solution 2: Without using space on hash, we can interleave the copy list with the original list, and set random pointers, then in the end break up the interleaving to restore the original list
+ * 
+ */
+public class Solution {
+    public RandomListNode copyRandomList(RandomListNode head) {
+        if (head == null) return null;
+        
+        // Interleave copy with origin
+        RandomListNode current = head;
+        while (current != null) {
+            RandomListNode copy = new RandomListNode(current.label);
+            copy.next = current.next;
+            current.next = copy;
+            current = copy.next;
+        }
+
+        // Assign the copy head to return
+        RandomListNode copyHead = head.next;
+        
+        // Set random pointers for the copy
+        current = head;
+        while (current != null) {
+            RandomListNode copy = current.next;
+            if (current.random != null) {
+                copy.random = current.random.next;
+            }
+            current = current.next.next;
+        }
+        
+        // Break the temp interleaving
+        current = head;
+        while (current != null) {
+            RandomListNode copy = current.next;
+            current.next = copy.next;
+            if (copy.next != null) {
+                copy.next = copy.next.next;    
+            }
+            current = current.next;
+        }
+        
+        return copyHead;
+    }
+}
 /**
  * A linked list is given such that each node contains an additional random pointer which could point to any node in the list or null.
 
