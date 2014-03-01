8ebec38787a77736cc473471854776732586acab
liuyingchao <liuyingchao@users.noreply.github.com>
2014-03-01 15:00:49 -0800

    Update subsets.java

--- subsets.java
+++ subsets.java
@@ -18,9 +18,37 @@ If S = [1,2,3], a solution is:
   []
 ]
  * */
-import java.util.ArrayList;
-import java.util.Arrays;
 
+// Refactor to get rid of unnecessary helper function
+public class Solution {
+    public ArrayList<ArrayList<Integer>> subsets(int[] s) {
+        ArrayList<ArrayList<Integer>> ret = new ArrayList<ArrayList<Integer>>();
+        ret.add(new ArrayList<Integer>());
+        if (s == null || s.length == 0) {
+        	return ret;
+        }
+        // Must sort first to automatically get each ArrayList in ascending order
+        Arrays.sort(s);
+        int N = s.length;
+        for (int i = 0; i < N; i++) {
+        	int newVal = s[i];
+        	int length = ret.size();
+        	for (int j = 0; j < length; j++) {
+        		ArrayList<Integer> member = ret.get(j);
+        		ArrayList<Integer> clone = new ArrayList<Integer>();
+        		for (Integer val : member) {
+        			clone.add(val);
+        		}
+        		clone.add(newVal);
+        		ret.add(clone);
+        	}        	
+        }
+        return ret;
+    }
+}
+
+
+// Poorly written version
 public class Solution {
     public ArrayList<ArrayList<Integer>> subsets(int[] s) {
         ArrayList<ArrayList<Integer>> ret = new ArrayList<ArrayList<Integer>>();
