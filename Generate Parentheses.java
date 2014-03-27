ccac8847c9f61d472efc2879931973df3ad1a7e4
liuyingchao <liuyingchao@users.noreply.github.com>
2014-03-27 10:10:34 -0700

    Refactor to cleaner thinking and get rid of global

--- Generate Parentheses.java
+++ Generate Parentheses.java
@@ -8,6 +8,8 @@
  For example, given n = 3, a solution set is:
  "((()))", "(()())", "(())()", "()(())", "()()()"
 
+Difficulty: Medium
+
  Solution: Place n left '(' and n right ')'.
            Cannot place ')' if there are no enough matching '('.
  
@@ -16,6 +18,33 @@
  we can also insert right as long as it's no more than left. Hence the 2 if conditions in the helper.
    
  */
+ 
+public class Solution {
+	// Use how many we have instead of how many more we can insert to be more straightforward
+    public ArrayList<String> generateParenthesis(int n) {
+        ArrayList<String> list = new ArrayList<String>();
+        if (n == 0) return list;
+        if (n < 0) throw new IllegalArgumentException("n must be non-negative");
+        char[] cA = new char[2*n];
+        process(n, list, cA, 0, 0);
+        return list;
+    }
+    
+    private void process(int n, List<String> list, char[] cA, int left, int right) {
+        if (right == n) {   // don't need to check against left, because we never try to go beyond n for left at the first place
+            list.add(String.valueOf(cA));
+            return;
+        }
+        if (right < left) {
+            cA[left+right] = ')';
+            process(n, list, cA, left, right+1);
+        }
+        if (left < n) {
+            cA[left + right] = '(';
+            process(n, list, cA, left+1, right);
+        }
+    }
+} 
 public class Solution {
 	ArrayList<String> result;
     public ArrayList<String> generateParenthesis(int n) {
