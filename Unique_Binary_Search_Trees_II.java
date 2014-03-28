a9926fb648e3036312f18a08508d2f50fe658afa
liuyingchao <liuyingchao@users.noreply.github.com>
2014-03-28 10:28:01 -0700

    Refactor

--- Unique_Binary_Search_Trees_II.java
+++ Unique_Binary_Search_Trees_II.java
@@ -1,14 +1,18 @@
-import java.util.ArrayList;
-
-
 /**
- * Definition for binary tree
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; left = null; right = null; }
- * }
+Given n, generate all structurally unique BST's (binary search trees) that store values 1...n.
+
+For example,
+Given n = 3, your program should return all 5 unique BST's shown below.
+
+   1         3     3      2      1
+    \       /     /      / \      \
+     3     2     1      1   3      2
+    /     /       \                 \
+   2     1         2                 3
+   
+ Difficulty: Hard
+ Solution: DP with ArrayList as cache entries. Notice we need dimension of n+2 by n+2 to cover the leftmost and rightmost children.
+ And the start>end logic is beautifully combined with boundary code to avoid any initialization.
  */
 public class Solution {
     private ArrayList<TreeNode>[][] table;
@@ -25,27 +29,20 @@ public class Solution {
         al.add(null);
         table[0][0] = al;
         
-        for (int i = 1; i <= n; i++) {
-        	al = new ArrayList<TreeNode>();
-        	TreeNode node = new TreeNode(i);
-        	al.add(node);
-        	table[i][i] = al;
-        }
-        
-        return getTrees(1, n, 0, n+1);
+        return getTrees(1, n);
     }
     
-    private ArrayList<TreeNode> getTrees(int start, int end, int lowerBound, int upperBound) {
+    private ArrayList<TreeNode> getTrees(int start, int end) {
     	ArrayList<TreeNode> al = new ArrayList<TreeNode>();
-    	if (start > end || start <= lowerBound || end >= upperBound) {
+    	if (start > end) {  // Unnecessary to initialize the diagonal nodes. We can rely on the code to set their left and right to null
     		al.add(null);
     	} else {
     		if (table[start][end] != null) {
-        		return table[start][end]; 
+        		return table[start][end];
         	}
     		for (int i = start; i <= end; i++) {
-            	ArrayList<TreeNode> leftTrees = getTrees(start, i - 1, lowerBound, i);
-            	ArrayList<TreeNode> rightTrees = getTrees(i + 1, end, i, upperBound);
+            	ArrayList<TreeNode> leftTrees = getTrees(start, i - 1);
+            	ArrayList<TreeNode> rightTrees = getTrees(i + 1, end);
             	for (TreeNode leftNode : leftTrees) {
             		for (TreeNode rightNode: rightTrees) {
             			TreeNode root = new TreeNode(i);
